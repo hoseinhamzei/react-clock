a5fcdde9e7a8245acdfb9dbd9f8f1acf55058ca5
Mohamad Hosein Hamzenejad <hnkn1998@gmail.com>
2024-05-05 10:30:31 +0000

    add basic storybook

---
+++ src/stories/AnalogClock.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { AnalogClock } from "../components/AnalogClock/AnalogClock";
+
+export default {
+    title: "Analog Clock",
+    component: AnalogClock
+}
+
+export const noCustomProps = () => <AnalogClock />
